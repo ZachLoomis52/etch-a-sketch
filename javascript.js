183f9705c1dd896c9c60fef275d64c51d41b2b09
zachloomis52 <zachloomis52@gmail.com>
2024-10-18 09:59:47 -0400

    Created hover function
    Used foreach method to call 2 functions for each .box class. When the mouse enters one of the class it turns red
    and when it leaves it turn blue.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,32 @@
 const container = document.querySelector(".container");
 
-for(var i = 0; i< 257; i++){
-    const box = document.createElement("div");
-    box.setAttribute("style", "border: 2px solid black; padding: 20px; max-width: 15px");
-    container.appendChild(box);
+for(var i = 0; i < 256; i++){
+    const boxes = document.createElement("div");
+    boxes.classList.add("box");
+    boxes.setAttribute("style", "border: 2px solid black; padding: 20px; max-width: 15px");
+    container.appendChild(boxes);
 };
+
+document.querySelectorAll(".box").forEach(element => {
+    element.addEventListener("mouseenter", () => {
+        element.style.backgroundColor = "red";
+    })
+
+    element.addEventListener("mouseleave", () => {
+    element.style.backgroundColor = "blue";
+})
+});
+ 
+
+
+
+
+/*divs.forEach(element => {"mouseenter", () => {
+    element.style.backgroundColor = "red";
+}});
+
+divs.addEventListener("mouseleave", () => {
+    divs.style.backgroundColor = "blue"
+});
+*/
 
